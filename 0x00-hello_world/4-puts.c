4b023e9144ae7cea246a6b46f9bd27cdf87726a1
safwana2 <alhassan0540221766@icloud.com>
2023-03-17 09:12:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program to display line using puts funtion
+ *
+ * Return:  0 (Success)
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
