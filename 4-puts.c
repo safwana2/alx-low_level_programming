9e91aa7378086e911ff3135365d1df15fcc9281b
safwana2 <alhassan0540221766@icloud.com>
2023-03-17 08:24:03 -0700

    4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - A  program to display line using the puts funtion
+ *
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"
+);
+return (0);
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+this is respository on c
