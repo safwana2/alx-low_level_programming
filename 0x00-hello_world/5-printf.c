154b65ac966f54377eca6c4e0d6d1258b3511758
safwana2 <alhassan0540221766@icloud.com>
2023-03-17 13:22:48 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - A program to display aline using the prinf funtion
+ * main - A program  that displays a line using printf statement
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
