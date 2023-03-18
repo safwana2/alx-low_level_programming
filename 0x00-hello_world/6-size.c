8c632d0098713745df7086c928208d60918b5558
safwana2 <alhassan0540221766@icloud.com>
2023-03-18 16:33:29 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-printf("size of a char:%lu bytes(s)\n", sizeof(char));
-printf("size of a int:%lu bytes(s)\n", sizeof(int));
-printf("size of a long int:%lu byte(s)\n", sizeof(long int));
-printf("size of a long long:%lu byte(s)\n", sizeof(long long));
-printf("size of a float:%lu byte(s)\n", sizeof(float));
+printf("size of a char:%ld bytes(s)\n", sizeof(char));
+printf("size of a int:%ld bytes(s)\n", sizeof(int));
+printf("size of a long int:%ld byte(s)\n", sizeof(long int));
+printf("size of a long long:%ld byte(s)\n", sizeof(long long));
+printf("size of a float:%ld byte(s)\n", sizeof(float));
 return (0);
 
 }
