69c276e13e41f5f00ac8c907a1a086a55be4f346
safwana2 <alhassan0540221766@icloud.com>
2023-03-18 18:48:52 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unisted.h>
+/**
+ * main - a program to print using fput
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\"-  Dora Kor
+par, 2015-10-19\", stdout);
+return (0);
+}
+
