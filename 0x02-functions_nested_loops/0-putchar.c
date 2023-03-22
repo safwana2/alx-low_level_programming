f6aec1431300c1c94a1cab5d6afd3ad48b9531b2
safwana2 <alhassan0540221766@icloud.com>
2023-03-21 18:41:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
-
 /**
  * main - check the code
  * Return: Always 0.
  */
-
 int main(void)
 
 {
-
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -19,6 +16,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
- return (0);
+return (0);
 
 }
