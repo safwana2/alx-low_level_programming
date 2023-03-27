974cff34cdd40533cd39392956de2742f0c9ca93
safwana2 <alhassan0540221766@icloud.com>
2023-03-27 05:59:44 -0700

    op

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  int _islower - check the code.
+ *
+ * Return: If true return 1 or return anything.
+ */
+
+int _islower(int c)
+{
+
+if (c >= 97 && c <= 127)
+{
+return (1);
+}
+return (0);
+}
+
